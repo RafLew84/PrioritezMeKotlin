573963ccf7c019b5c9f7fd73b24241b656cced72
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-08-15 07:56:08 +0200

    fragments layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     //Fragment
     implementation ("androidx.fragment:fragment-ktx:1.6.1")
 
+    implementation("androidx.cardview:cardview:1.0.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
